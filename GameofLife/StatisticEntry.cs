87d168ef2cf1259174c874a46e0fdaf9370832fb
Zakant <zakant@gmx.de>
2014-05-08 16:05:25 +0200

    frmStatistic und StatisticEntry erstellt

---
+++ GameofLife/StatisticEntry.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameofLife
+{
+    class StatisticEntry
+    {
+
+        public int Generation { get; protected set; }
+        public int LivingCells { get; protected set; }
+
+
+        public StatisticEntry(int Generation, int LivingCells)
+        {
+            this.Generation = Generation;
+            this.LivingCells = LivingCells;
+        }
+    }
+}
