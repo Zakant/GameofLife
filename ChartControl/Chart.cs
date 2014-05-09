da032c38b7b0a97d9313c3578acc2ca8481492d5
Zakant <zakant@gmx.de>
2014-05-09 19:49:41 +0200

    Grundlegende Klassen für das ChartControl erstellt

---
+++ ChartControl/Chart.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ChartControl
+{
+    class Chart : Panel
+    {
+    }
+}
---
+++ ChartControl/ChartPoint.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace ChartControl
+{
+    class ChartPoint
+    {
+        public int XValue { get; set; }
+        public int YValue { get; set; }
+
+        public ChartPoint(Point p)
+        {
+            XValue = p.X;
+            YValue = p.Y;
+        }
+        public ChartPoint(int x, int y)
+        {
+            XValue = x;
+            YValue = y;
+        }
+    }
+}
