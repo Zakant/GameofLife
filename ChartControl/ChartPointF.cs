4651ba7f9d93f4fd55533d79e999935e9ebab4e1
Zakant <zakant@gmx.de>
2014-05-09 19:51:52 +0200

    Klasse ChartPointF implementiert

--- ChartControl/ChartPointF.cs
+++ ChartControl/ChartPointF.cs
@@ -2,10 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Drawing;
 
 namespace ChartControl
 {
     class ChartPointF
     {
+        public float XValue { get; set; }
+        public float YValue { get; set; }
+
+        public ChartPointF(PointF p)
+        {
+            XValue = p.X;
+            YValue = p.Y;
+        }
+        public ChartPointF(float x, float y)
+        {
+            XValue = x;
+            YValue = y;
+        }
     }
 }
