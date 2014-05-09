2a0fc5a18c79e73973c43d84c47b9a43215e12de
Zakant <zakant@gmx.de>
2014-05-09 20:30:21 +0200

    ChartControl um grundlegende Felder erweitert

--- ChartControl/Chart.cs
+++ ChartControl/Chart.cs
@@ -11,10 +11,28 @@ namespace ChartControl
 {
     class Chart : Panel
     {
-        
+        private BindingList<ChartPath> _chartpath = new BindingList<ChartPath>();
+        private BindingList<ChartPathF> _chartpathf = new BindingList<ChartPathF>();
 
         public Chart()
         {
+            _chartpath.ListChanged += HandleListChanged;
+            _chartpathf.ListChanged += HandleListChanged;
+            this.DoubleBuffered = true;
+        }
+
+        void HandleListChanged(object sender, ListChangedEventArgs e)
+        {
+        }
+
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+        }
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            base.OnPaintBackground(e);
         }
     }
 }
--- ChartControl/ChartPoint.cs
+++ ChartControl/ChartPoint.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Drawing;
 using System.ComponentModel;
-using System.ComponentModel;
 
 namespace ChartControl
 {
